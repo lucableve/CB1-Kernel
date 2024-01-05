1b16b96b56ba6e8d540611547f8cb1baec3fbcbd
luca <info@lucableve.it>
2024-01-05 02:09:48 +0100

    add fix driver tsc

--- kernel/drivers/input/touchscreen/tsc2007_core.c
+++ kernel/drivers/input/touchscreen/tsc2007_core.c
@@ -70,22 +70,23 @@ static void tsc2007_read_values(struct tsc2007 *tsc, struct ts_event *tc)
 
 u32 tsc2007_calculate_resistance(struct tsc2007 *tsc, struct ts_event *tc)
 {
-	u32 rt = 0;
+    u32 rt = 0;
+    if (tc->x == MAX_12BIT)
+        tc->x = 0;
 
-	/* range filtering */
-	if (tc->x == MAX_12BIT)
-		tc->x = 0;
+    if (likely(tc->x && tc->z1)) {
 
-	if (likely(tc->x && tc->z1)) {
-		/* compute touch resistance using equation #1 */
-		rt = tc->z2 - tc->z1;
-		rt *= tc->x;
-		rt *= tsc->x_plate_ohms;
-		rt /= tc->z1;
-		rt = (rt + 2047) >> 12;
-	}
+        if (likely(tsc->y_plate_ohms)) {
+            rt = (tsc->x_plate_ohms * tc->x / 4096) * ((4096 / tc->z1) - 1) - tsc->y_plate_ohms * (1 - tc->y / 4096);
+        }
 
-	return rt;
+        if (likely(tc->y && tsc->x_plate_ohms && tsc->y_plate_ohms)) {
+            dev_info(&tsc->client->dev, "RT: (%4d) | X Plate: (%4d) | Y Plate: (%4d) | TC Z1: (%4d) | TC Z2: (%4d) | TC X: (%4d) | TC Y: (%4d)",
+                     rt, tsc->x_plate_ohms, tsc->y_plate_ohms, tc->z1, tc->z2, tc->x, tc->y);
+        }
+    }
+
+    return rt;
 }
 
 bool tsc2007_is_pen_down(struct tsc2007 *ts)
@@ -189,18 +190,11 @@ static irqreturn_t tsc2007_soft_poll(int irq, void *handle)
 
 		rt = tsc2007_calculate_resistance(ts, &tc);
 
-		if (rt == 0 || rt == 256) {
+        /* range >= 0 && <= 4096 */
+		if (rt > 0 && rt <= ts->max_rt) {
+		        dev_info(&ts->client->dev, "[TS-EVENT] Real touch event trigger rt: (%4d)",rt);
+		        dev_info(&ts->client->dev, "[TS-EVENT] TC X: (%4d)\nTC Y: (%4d)",tc.x,tc.y);
 
-			/*
-				* Sample found inconsistent by debouncing or pressure is
-				* beyond the maximum. Don't report it to user space,
-				* repeat at least once more the measurement.
-				*/
-			dev_dbg(&ts->client->dev, "ignored pressure %d\n", rt);
-
-		} else {
-
-			if (rt < ts->rt_thr) {
 
 				dev_dbg(&ts->client->dev,
 					"DOWN point(%4d,%4d), resistance (%4u)\n",
@@ -216,7 +210,7 @@ static irqreturn_t tsc2007_soft_poll(int irq, void *handle)
 				input_sync(input);
 				ts->touched = 1;
 
-			} else if (ts->touched == 1) {
+		} else {
 
 				dev_dbg(&ts->client->dev, "UP\n");
 
@@ -224,10 +218,7 @@ static irqreturn_t tsc2007_soft_poll(int irq, void *handle)
 				input_report_abs(input, ABS_PRESSURE, 0);
 				input_sync(input);
 				ts->touched = 0;
-			}
 		}
-
-
 	}
 
 	return IRQ_HANDLED;
@@ -339,6 +330,13 @@ static int tsc2007_probe_properties(struct device *dev, struct tsc2007 *ts)
 		ts->x_plate_ohms = val32;
 	} else {
 		dev_err(dev, "Missing ti,x-plate-ohms device property\n");
+		return -EINVAL;
+	}
+
+	if (!device_property_read_u32(dev, "ti,y-plate-ohms", &val32)) {
+		ts->y_plate_ohms = val32;
+	} else {
+		dev_err(dev, "Missing ti,y-plate-ohms device property\n");
 		return -EINVAL;
 	}
 
